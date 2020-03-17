1a2ea38df53a023cbb8ac5462080a762aac1679b
sky <jingcai.wei@163.com>
2020-03-17 22:01:38 +0800

    添加测试类

---
+++ core/src/main/java/com/sky/xposed/core/component/EventManager.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2020 The sky Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.sky.xposed.core.component;
+
+import com.sky.xposed.core.base.AbstractComponent;
+import com.sky.xposed.core.interfaces.XEvent;
+import com.sky.xposed.core.interfaces.XEventManager;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by sky on 2020-03-17.
+ */
+public class EventManager extends AbstractComponent implements XEventManager {
+
+    private Map<Class<? extends XEvent>, List<XEvent>> mClassListMap = new HashMap<>();
+
+    @Override
+    public <T extends XEvent> void register(Class<T> tClass, T listener) {
+
+        List<XEvent> events = mClassListMap.get(tClass);
+
+        if (events == null) {
+            events = new ArrayList<>();
+            mClassListMap.put(tClass, events);
+        }
+
+        events.add(listener);
+    }
+
+    @Override
+    public <T extends XEvent> void notice(Class<T> tClass, Callback<T> callback) {
+
+        List<XEvent> events = mClassListMap.get(tClass);
+
+        if (events == null) return;
+
+        for (XEvent event : events) {
+            callback.onHandler(tClass.cast(event));
+        }
+    }
+}
